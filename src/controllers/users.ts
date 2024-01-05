d4dbbc81cee51165535c47eed8d110bc7b8f2d46
Avellano30 <avellanolimuel30@gmail.com>
2024-01-06 05:40:52 +0800

    first commit

---
+++ src/controllers/users.ts
@@ -0,0 +1,47 @@
+import express from 'express';
+
+import { deleteUserById, getUserById, getUsers } from '../db/users';
+
+export const getAllUsers = async (req: express.Request, res: express.Response) => {
+    try {
+        const users = await getUsers();
+        
+        return res.status(200).json(users);
+    } catch (err) {
+         console.error(err);
+         return res.sendStatus(err);
+    }
+};
+
+export const deleteUser = async (req: express.Request, res: express.Response) => {
+    try {
+        const { userId } = req.params;
+        
+        const deleteUser = await deleteUserById(userId);
+
+        return res.json(deleteUser);
+    } catch (err) {
+         console.error(err);
+         return res.sendStatus(err);
+    }
+};
+
+export const updateUser = async (req: express.Request, res: express.Response) => {
+    try {
+        const { userId } = req.params;
+        const { username } = req.body;
+
+        if(!username) {
+            return res.sendStatus(400);
+        }
+        const user = await getUserById(userId);
+
+        user.username = username;
+        await user.save();
+
+        return res.status(200).json(user).end();
+    } catch (err) {
+         console.error(err);
+         return res.sendStatus(err);
+    }
+};
