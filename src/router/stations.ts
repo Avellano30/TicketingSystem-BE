5ab3234deb27ca7ba11eb297f1532655e60d5552
Avellano30 <avellanolimuel30@gmail.com>
2024-02-03 04:51:09 +0800

    update and delete dynamically

--- src/router/stations.ts
+++ src/router/stations.ts
@@ -4,7 +4,7 @@ import { getAllStations, createNewStation, updateStation, deleteStation, getStat
 import { isAuthenticated } from '../middlewares';
 
 export default (router: express.Router) => {
-    router.get('/stations', isAuthenticated, getAllStations);
+    router.get('/stations', getAllStations);
     router.post('/stations/create', isAuthenticated, createNewStation);
     router.delete('/stations/:stationId', isAuthenticated, deleteStation);
     router.patch('/stations/:stationId', isAuthenticated, updateStation);
